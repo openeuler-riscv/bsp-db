ad268899edeed499a89cd50bf4ccad8128b156ef
Hangfan Li <lihangfan@iscas.ac.cn>
2025-03-04 10:02:25 +0800

    products: Add support for MUSE Card

--- products/spacemit/vendor.mk
+++ products/spacemit/vendor.mk
@@ -1,4 +1,5 @@
 SPACEMIT_DIR := $(patsubst %/,%,$(dir $(lastword $(MAKEFILE_LIST))))
 
 BOARDS += $(SPACEMIT_DIR)/muse_pi.yml
+BOARDS += $(SPACEMIT_DIR)/muse_card.yml
 SOCS += $(SPACEMIT_DIR)/m1.yml
