b4543fb5a5dec2ef708a2bca144d145b1506b51b
Hangfan Li <lihangfan@iscas.ac.cn>
2025-03-05 09:35:57 +0800

    products: merge SpacemiT K1&M1

--- products/spacemit/vendor.mk
+++ products/spacemit/vendor.mk
@@ -2,5 +2,4 @@ SPACEMIT_DIR := $(patsubst %/,%,$(dir $(lastword $(MAKEFILE_LIST))))
 
 BOARDS += $(SPACEMIT_DIR)/muse_pi.yml
 BOARDS += $(SPACEMIT_DIR)/muse_card.yml
-SOCS += $(SPACEMIT_DIR)/m1.yml
-SOCS += $(SPACEMIT_DIR)/k1.yml
+SOCS += $(SPACEMIT_DIR)/k1_m1.yml
