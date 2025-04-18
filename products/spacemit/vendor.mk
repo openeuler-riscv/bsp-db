83718766e8107f84faf367b338bf6e9a08a42306
Hangfan Li <lihangfan@iscas.ac.cn>
2025-04-18 14:27:19 +0800

    products: Add Muse Box

--- products/spacemit/vendor.mk
+++ products/spacemit/vendor.mk
@@ -2,4 +2,6 @@ SPACEMIT_DIR := $(patsubst %/,%,$(dir $(lastword $(MAKEFILE_LIST))))
 
 BOARDS += $(SPACEMIT_DIR)/muse_pi.yml
 BOARDS += $(SPACEMIT_DIR)/muse_card.yml
+BOARDS += $(SPACEMIT_DIR)/muse_box.yml
+
 SOCS += $(SPACEMIT_DIR)/k1_m1.yml
