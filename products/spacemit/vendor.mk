d5caf57b80ed8a23ebad91c4c6ef2a7ca7768727
Hangfan Li <lihangfan@iscas.ac.cn>
2025-04-21 11:56:07 +0800

    products: Add Muse Pi Pro

--- products/spacemit/vendor.mk
+++ products/spacemit/vendor.mk
@@ -3,5 +3,6 @@ SPACEMIT_DIR := $(patsubst %/,%,$(dir $(lastword $(MAKEFILE_LIST))))
 BOARDS += $(SPACEMIT_DIR)/muse_pi.yml
 BOARDS += $(SPACEMIT_DIR)/muse_card.yml
 BOARDS += $(SPACEMIT_DIR)/muse_box.yml
+BOARDS += $(SPACEMIT_DIR)/muse_pi_pro.yml
 
 SOCS += $(SPACEMIT_DIR)/k1_m1.yml
