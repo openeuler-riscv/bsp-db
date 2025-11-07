c3f4a2e6d2aa846b953998b069bbec2f51dddeb8
Hangfan Li <lihangfan@iscas.ac.cn>
2025-11-07 18:05:36 +0800

    distro: Supports openEuler 24.03-LTS-SP1 on SiFive HiFive Premier P550

--- distros/openeuler/24.03sp1/release.mk
+++ distros/openeuler/24.03sp1/release.mk
@@ -6,3 +6,4 @@ IMAGESUITES += $(OPENEULER_24.03_SP1_DIR)/imagesuites/qemu-virt.yml
 IMAGESUITES += $(OPENEULER_24.03_SP1_DIR)/imagesuites/sg2042-official-linuxboot.yml
 IMAGESUITES += $(OPENEULER_24.03_SP1_DIR)/imagesuites/spacemit-k1-uboot-bootstd.yml
 IMAGESUITES += $(OPENEULER_24.03_SP1_DIR)/imagesuites/zhihe-a210-extlinux.yml
+IMAGESUITES += $(OPENEULER_24.03_SP1_DIR)/imagesuites/eswin-eic7700x-extlinux.yml
