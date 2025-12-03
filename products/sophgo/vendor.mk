39222431eddc95e07fc1e4327b6d9e15447b54a6
Hangfan Li <lihangfan@iscas.ac.cn>
2025-12-03 12:52:53 +0800

    boards: add StarFive VisionFive
    Resolves: https://github.com/openeuler-riscv/bsp-db/issues/34

--- products/sophgo/vendor.mk
+++ products/sophgo/vendor.mk
@@ -1,3 +1,6 @@
 SOPHON_DIR := $(patsubst %/,%,$(dir $(lastword $(MAKEFILE_LIST))))
 
 SOCS += $(SOPHON_DIR)/sg2042.yml
+SOCS += $(SOPHON_DIR)/sg2044.yml
+
+BOARDS += $(SOPHON_DIR)/sg2044-evb.yml
--- products/starfive/vendor.mk
+++ products/starfive/vendor.mk
@@ -1,4 +1,7 @@
 STARFIVE_DIR := $(patsubst %/,%,$(dir $(lastword $(MAKEFILE_LIST))))
 
-BOARDS += $(STARFIVE_DIR)/visionfive2.yml
+SOCS += $(STARFIVE_DIR)/jh7100.yml
 SOCS += $(STARFIVE_DIR)/jh7110.yml
+
+BOARDS += $(STARFIVE_DIR)/visionfive.yml
+BOARDS += $(STARFIVE_DIR)/visionfive2.yml
