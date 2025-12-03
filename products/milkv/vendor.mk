55a8fe89e0b8aed3fb8f42fdf7edb4188bf5957b
Hangfan Li <lihangfan@iscas.ac.cn>
2025-12-03 12:14:36 +0800

    boartds: add Milk-V Mars
    Resolves: https://github.com/openeuler-riscv/bsp-db/issues/12

--- products/milkv/vendor.mk
+++ products/milkv/vendor.mk
@@ -3,3 +3,4 @@ MILKV_DIR := $(patsubst %/,%,$(dir $(lastword $(MAKEFILE_LIST))))
 BOARDS += $(MILKV_DIR)/pioneer.yml
 BOARDS += $(MILKV_DIR)/megrez.yml
 BOARDS += $(MILKV_DIR)/jupter.yml
+BOARDS += $(MILKV_DIR)/mars.yml
