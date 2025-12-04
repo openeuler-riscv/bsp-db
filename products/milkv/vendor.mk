5f8de7d8147dc072affa6849d3e08a32c5591567
Jvle <keke.oerv@isrc.iscas.ac.cn>
2025-12-04 15:50:45 +0800

    jupiter: fix name from jupter to jupiter
    Signed-off-by: Jvle <keke.oerv@isrc.iscas.ac.cn>

--- products/milkv/vendor.mk
+++ products/milkv/vendor.mk
@@ -2,6 +2,6 @@ MILKV_DIR := $(patsubst %/,%,$(dir $(lastword $(MAKEFILE_LIST))))
 
 BOARDS += $(MILKV_DIR)/pioneer.yml
 BOARDS += $(MILKV_DIR)/megrez.yml
-BOARDS += $(MILKV_DIR)/jupter.yml
+BOARDS += $(MILKV_DIR)/jupiter.yml
 BOARDS += $(MILKV_DIR)/mars.yml
 BOARDS += $(MILKV_DIR)/meles.yml
